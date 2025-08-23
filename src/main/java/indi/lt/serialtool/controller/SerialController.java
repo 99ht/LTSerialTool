34ce3fd0bfc846a04c03e8ac6fb6ce5cc1d3d765
Nonoas <1356955215@qq.com>
2025-08-23 10:26:52 +0800

    添加切换模式菜单

--- src/main/java/indi/lt/serialtool/controller/SerialController.java
+++ src/main/java/indi/lt/serialtool/controller/SerialController.java
@@ -3,9 +3,12 @@ package indi.lt.serialtool.controller;
 import indi.lt.serialtool.view.SerialReceivePane;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -34,5 +37,15 @@ public class SerialController implements Initializable {
         SerialReceivePane serialReceivePane1 = new SerialReceivePane("串口1:");
         SerialReceivePane serialReceivePane2 = new SerialReceivePane("串口2:");
         spMain.getItems().addAll(serialReceivePane1, serialReceivePane2);
+    }
+
+    @FXML
+    private void changeToReceiveMode(){
+        rootPane.setCenter(spMain);
+    }
+
+    @FXML
+    private void changeToSendMode(){
+        rootPane.setCenter(new StackPane(new Button("我是你大爷的发送模式")));
     }
 }
